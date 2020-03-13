c86964b91dcf7d1951a016920a54a4dcc62072b6
Maciej Sznurawa <msznurawa@gmail.com>
2020-03-13 16:54:08 +0100

    Add draft portfolio code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Portfolio
+
+Draft version
---
+++ js/script.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const click = (target, callback) => {
+  target.addEventListener('click', (e) => {
+    e.preventDefault();
+    return callback(e)
+  });
+}
+
+const hideMobileMenu = () => {
+  mobileMenu.classList.toggle('mobile-hidden');
+};
+
+const hamburgerButton = document.querySelector('.hamburger-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+hamburgerButton.addEventListener('click', (e) => {
+  hideMobileMenu();
+});
+
+const mobileMenuItems = document.querySelectorAll('.mobile-menu__list-item a');
+const documentSections = document.querySelectorAll('*[id^="section"]');
+const documentSectionsArray = [...documentSections];
+
+const scrollToSection = (i) => {
+  documentSectionsArray[i].scrollIntoView({
+    bahavior: 'smooth'
+  });
+};
+
+mobileMenuItems.forEach((item, index) => {
+  click(item, () => {
+    scrollToSection(index);
+    hideMobileMenu();
+  })
+});
+
+
+/* BOX_NUMBERS ANIMATIONS */
+
+const animateSectionNumberBox = () => {
+  let elements;
+  let windowHeight;
+
+  const init = () => {
+    elements = document.querySelectorAll('.animated');
+    windowHeight = window.innerHeight;
+    addEventHandlers();
+    checkPosition();
+  }
+
+  const addEventHandlers = () => {
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+  }
+
+  const checkPosition = () => {
+    for (let i = 0; i < elements.length; i++) {
+      let positionFromTop = elements[i].getBoundingClientRect().top;
+
+      if (positionFromTop - windowHeight <= 0) {
+        elements[i].className = elements[i].className.replace(
+          'animated',
+          'visible'
+        );
+      }
+  
+      if ((positionFromTop - windowHeight > 1) || (positionFromTop < 0)) {
+        elements[i].className = elements[i].className.replace(
+          'visible',
+          'animated'
+        );
+      }
+    }
+  };
+
+  init();
+  checkPosition();
+
+};
+
+animateSectionNumberBox();
